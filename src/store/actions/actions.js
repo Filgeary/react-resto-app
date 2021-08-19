b1f65bcd7d1d6784833c217fe8cc5586864daab0
Georgiy Filippov <g.filippov777@gmail.com>
2021-08-19 11:34:57 +0500

    add `ActionType`, new actions, extend `menuReducer`, add selector functions

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,6 +1,25 @@
-export const menuLoaded = data => {
+export const ActionType = {
+  // DATA
+  LOAD_MENU_REQUEST: 'LOAD_MENU_REQUEST',
+  LOAD_MENU_SUCCESS: 'LOAD_MENU_SUCCESS',
+  LOAD_MENU_FAILURE: 'LOAD_MENU_FAILURE',
+}
+
+export const actionLoadMenuRequest = () => {
   return {
-    type: 'MENU_LOADED',
-    payload: data,
+    type: ActionType.LOAD_MENU_REQUEST,
+  }
+}
+
+export const actionLoadMenuSuccess = menuList => {
+  return {
+    type: ActionType.LOAD_MENU_SUCCESS,
+    payload: menuList,
+  }
+}
+
+export const actionLoadMenuFailure = () => {
+  return {
+    type: ActionType.LOAD_MENU_FAILURE,
   }
 }

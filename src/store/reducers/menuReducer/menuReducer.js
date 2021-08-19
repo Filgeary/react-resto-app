f13a4963f278ca7abc493202042d44d63cc1f216
Georgiy Filippov <g.filippov777@gmail.com>
2021-08-20 03:03:02 +0500

    add `combineReducers`

---
+++ src/store/reducers/menuReducer/menuReducer.js
@@ -0,0 +1,42 @@
+import { ActionType } from '../../actions/actions'
+
+const initialState = {
+  menu: [],
+  isLoading: false,
+  isError: false,
+}
+
+const menuReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.LOAD_MENU_SUCCESS:
+      return {
+        ...state,
+        menu: action.payload,
+        isLoading: false,
+        isError: false,
+      }
+
+    case ActionType.LOAD_MENU_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      }
+
+    case ActionType.LOAD_MENU_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      }
+
+    default:
+      return state
+  }
+}
+
+export const selectMenu = state => state.menu
+export const selectIsLoading = state => state.isLoading
+export const selectIsError = state => state.isError
+
+export default menuReducer

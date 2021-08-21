92671f68a48a16dc77b1b914dffe36a55ba5a2dc
Georgiy Filippov <g.filippov777@gmail.com>
2021-08-21 22:10:53 +0500

    refactor - add Link like separate button

--- src/components/menu-list-item/menu-list-item.jsx
+++ src/components/menu-list-item/menu-list-item.jsx
@@ -7,19 +7,23 @@ const MenuListItem = ({ menuItem }) => {
 
   return (
     <li className="menu__item">
-      <Link to={`/menu/${id}`}>
-        <div className="menu__title">
-          {title} / {id}
-        </div>
-        <img className="menu__img" src={url} alt={title}></img>
-        <div className="menu__category">
-          Category: <span>{category}</span>
-        </div>
-        <div className="menu__price">
-          Price: <span>{price}$</span>
-        </div>
+      <div className="menu__title">
+        {title} / {id}
+      </div>
+      <img className="menu__img" src={url} alt={title}></img>
+      <div className="menu__category">
+        Category: <span>{category}</span>
+      </div>
+      <div className="menu__price">
+        Price: <span>{price}$</span>
+      </div>
+
+      <div className="menu__btn-wrapper">
         <button className="menu__btn">Add to cart</button>
-      </Link>
+        <Link to={`/menu/${id}`} className="menu__btn">
+          Show more
+        </Link>
+      </div>
     </li>
   )
 }

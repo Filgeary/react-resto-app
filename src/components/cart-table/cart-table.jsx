25a344f6c395b16d925be4fb67ed6bce9e8f1192
Georgiy Filippov <g.filippov777@gmail.com>
2021-08-23 03:49:58 +0500

    add `handleRemoveFromCart` to `CartTable`

--- src/components/cart-table/cart-table.jsx
+++ src/components/cart-table/cart-table.jsx
@@ -1,21 +1,43 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { actionRemoveItemFromCart } from '../../store/actions/actions'
+import { selectCartList } from '../../store/reducers/menuReducer/menuReducer'
 import './cart-table.scss'
 
 const CartTable = () => {
+  const dispatch = useDispatch()
+
+  const handleRemoveFromCart = id => {
+    dispatch(actionRemoveItemFromCart(id))
+  }
+
+  const cartList = useSelector(selectCartList)
+
   return (
     <>
       <div className="cart__title">Your Order:</div>
       <div className="cart__list">
-        <div className="cart__item">
-          <img
-            src="https://static.1000.menu/img/content/21458/-salat-cezar-s-kr-salat-cezar-s-krevetkami-s-maionezom_1501173720_1_max.jpg"
-            className="cart__item-img"
-            alt="Cesar salad"
-          ></img>
-          <div className="cart__item-title">Cesar salad</div>
-          <div className="cart__item-price">12$</div>
-          <div className="cart__close">&times;</div>
-        </div>
+        {cartList.length > 0 ? (
+          cartList.map(item => {
+            const { id, title, url, price } = item
+
+            return (
+              <div key={id} className="cart__item">
+                <img src={url} className="cart__item-img" alt={title}></img>
+                <div className="cart__item-title">{title}</div>
+                <div className="cart__item-price">{price}$</div>
+                <div
+                  className="cart__close"
+                  onClick={() => handleRemoveFromCart(id)}
+                >
+                  &times;
+                </div>
+              </div>
+            )
+          })
+        ) : (
+          <p className="cart__empty-desc">Your Cart is empty!</p>
+        )}
       </div>
     </>
   )
